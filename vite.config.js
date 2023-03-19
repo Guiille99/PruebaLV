115e4b2bce5dcec2498bd072d11bb3b0dfffdd4a
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-03-19 15:13:40 +0100

    commit 9

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js', 'resources/js/bootstrap.js', 'resources/js/jquery-3.6.3.js', 'resources/js/validation_form.js', 'resources/js/font-awesome.js', 'resources/js/datatables.min.js'],
+            input: ['resources/css/app.scss', 'resources/js/app.js', 'resources/js/bootstrap.js', 'resources/js/validation_form.js', 'resources/js/font-awesome.js', 'resources/js/datatables.min.js'],
             refresh: true,
         }),
     ],
