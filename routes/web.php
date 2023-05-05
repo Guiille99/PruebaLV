6cd66d19747e19efb824a8907ccea95fc1b03253
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-05-05 18:29:45 +0200

    acutalizacion

--- routes/web.php
+++ routes/web.php
@@ -112,7 +112,10 @@ Route::controller(PedidoController::class)->group(function(){
     Route::put("cancelar-pedido/{idPedido}", "cancelaPedido")->middleware('auth')->name('order.cancel');
 });
 
-Route::get('wishlist', WishlistComponent::class);
+Route::get('/livewire/message/wishlist', WishlistComponent::class);
+Route::post('/livewire/message/wishlist/', WishlistComponent::class);
+Route::get('/wishlist', WishlistComponent::class);
+Route::post('/wishlist', WishlistComponent::class);
 
 Route::post('enviar-correo', function() 
 {
