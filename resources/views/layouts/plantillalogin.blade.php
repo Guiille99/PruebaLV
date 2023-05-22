47a48636abfd3316a68a009fa4b1ea925bf8607b
Guillermo <guillermo.jarana-barragan@iesruizgijon.com>
2023-05-22 18:47:49 +0200

    prueba jquery min

--- resources/views/layouts/plantillalogin.blade.php
+++ resources/views/layouts/plantillalogin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield("title")</title>
     <link rel="shortcut icon" href="{{asset('uploads/logo.ico')}}" type="image/x-icon">
-    <script src="{{asset('build/assets/jquery-3.6.3.js')}}"></script>
+    <script src="{{asset('build/assets/jquery-3.6.3.min.js')}}"></script>
     @vite(["resources/css/app.scss", "resources/js/app.js", "resources/js/validation_form.js"])
 </head>
 <body>
